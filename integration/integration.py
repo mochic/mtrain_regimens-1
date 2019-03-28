a02978d58fa028f3457d55f327ef4f7c85c8b88b
christopher mochizuki <mochic808@gmail.com>
2019-03-28 13:55:02 -0700

    temp for now...

--- integration/integration.py
+++ integration/integration.py
@@ -1,30 +1,57 @@
 # python integration test runner...control?
 # no idea why this code looks so weird...~.~
 import logging
+import regex
 import requests
 import shutil
 import subprocess
 import time
 
 
-def init_services(mtrain_root):
-    # daemon = subprocess.Popen(
-    #     ['docker-compose', 'up', ], 
-    # )
-    subprocess.run(
+def init_services():
+    process = subprocess.run(
         'docker-compose up -d',
         check=True,
         shell=True,
+        stderr=subprocess.PIPE,
+    ) 
+    # writes to stderr regardless of whether or not 
+    # containers are already running
+    if process.stderr:
+        errs = process.stderr.splitlines()
+
+        # janky but least resistance way?
+        if len(errs) == 2:  # containers already started
+            db_container = errs[0].split()[0]  # extract container name from each statement
+            api_container = errs[1].split()[0]
+            return api_container, db_container,
+        else if len(errs) == 8:  # starting the containers
+            return api_container, db_container,
+        else:
+            raise ValueError('unexpected return from docker-compose')
+
+
+def kill_services()
+    return subprocess.run(
+       'docker-compose down',
+       check=True,
+       shell=True, 
     )
-    logging.info('waiting for services to be available...', )
+
+
+def mtrain_healthcheck(
+    mtrain_root,
+    max_wait=600,  # ~10 minutes in seconds
+):
     tic = time.time()
-    while time.time() - tic > 600:  # ~10 minutes max wait time
-        if requests.get(mtrain_root) \
-                .status_code == 200:
-            break
-
-        time.sleep(5)
-
+    while time.time() - tic > max_wait:
+        if requests.get(mtrain_root).status_code == 200:
+            return True
+        
+        time.sleep(5)  # wait for a little if it fails
+    else:
+        return False
+    
 
 def stop_services():
     subprocess.run(
@@ -36,10 +63,10 @@ def stop_services():
 
 def init_user(
     username, 
-    password, 
+    password,
     mtrain_api_container,
 ):
-    subprocess.run(
+   subprocess.run(
         'docker exec {container_name} {command}'.format(
             container_name=mtrain_api_container,
             command='python ./scripts/init_user_script.py {username} {password}' \
@@ -61,7 +88,7 @@ def init_assets(*training_outputs):
     for training_output in training_outputs:
         shutil.copyfile(
             training_output,
-            './tests/assets/training_output.pkl'
+            './tests/assets/training_output.pkl',
         )
 
 
@@ -77,10 +104,17 @@ def run_tests():
 if __name__ == '__main__':
     import os
 
-    init_services(
+    logging.info('initializing services...')
+    init_services()
+    logging.info('services initialized!')
+    logging.info('waiting for services to be available...', )
+    healthy = mtrain_healthcheck(
         mtrain_root=os.environ['MTRAIN_ROOT'],
     )
-
+    logging.info('mtrain service health...%s' % healthy)
+    if not healthy:
+        raise Exception('mtrain service isnt healthy')
+        
     try:
         init_user( 
             username=os.environ['MTRAIN_USERNAME'], 
@@ -90,7 +124,9 @@ if __name__ == '__main__':
     except:
         pass  # todo make more elegant
 
-    init_assets(os.environ['TRAINING_OUTPUT'], )
+    init_assets(
+        os.environ['TRAINING_OUTPUT'], 
+    )
 
     run_tests()
 
